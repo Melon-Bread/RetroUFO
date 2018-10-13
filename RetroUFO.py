0ce1122e2c9d04bd2aeeb252327f802da7a384a7
Melon Bread <bread.melon@gmail.com>
2018-10-13 15:37:20 -0400

    Initial Commit

---
+++ RetroUFO.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Grabs the latest version of every libretro core from the build bot.
+"""
+
+__author__ = "Melon Bread"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
